72d2d47175ed5ddd6f19beb653d6f398f8e979dd
creo.cons <creo.cons@bene.it>
2024-12-28 11:10:41 +0100

    dgfg

--- src/components/common.js
+++ src/components/common.js
@@ -1,16 +1,27 @@
 import { BounceLoader } from "react-spinners"
 
+/*
+            <div className={'mainContainer'}>
+                <div className={'titleContainer'}>
+                    <div>
+                        <div className='spinner-center'>
+                            <BounceLoader loading={props.loading} size={50} color="#123abc" speedMultiplier={1.5} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+*/
 const Spinner = (props) => {
     return (
-        <div className={'mainContainer'}>
-        <div className={'titleContainer'}>
-            <div>
+        <>
+
+
+            <div className="flex min-h-screen items-center justify-center bg-gradient-to-br from-purple-600 to-blue-500">
                 <div className='spinner-center'>
                     <BounceLoader loading={props.loading} size={50} color="#123abc" speedMultiplier={1.5} />
                 </div>
             </div>
-        </div>
-    </div>
+        </>
     )
 }
 
